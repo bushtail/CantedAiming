96ba62ff90a4a9458de6fcfe46f15e96ff69cc72
bushtail <evannosich@icloud.com>
2025-11-02 20:15:08 -0500

    incompatible with self

--- CantedAiming/Canted.cs
+++ CantedAiming/Canted.cs
@@ -9,9 +9,15 @@ namespace CantedAiming;
 // ReSharper disable once NotAccessedPositionalProperty.Global
 public record Canted(HashSet<MongoId> filter) : NewItemFromCloneDetails
 {
-    public override MongoId? ItemTplToClone { get; set; } = ItemTpl.COLLIMATOR_WALTHER_MRS_REFLEX_SIGHT;
     private const string _itemTpl = "69069986473b8fd5e80545f3";
-
+    
+    public override MongoId? ItemTplToClone { get; set; } = ItemTpl.COLLIMATOR_WALTHER_MRS_REFLEX_SIGHT;
+    public override string? NewId { get; set; } = _itemTpl;
+    public override string? ParentId { get; set; } = "55818ad54bdc2ddc698b4569";
+    public override string? HandbookParentId { get; set; } = "5b5f742686f774093e6cb4ff";
+    public override double? FleaPriceRoubles { get; set; } = 10;
+    public override double? HandbookPriceRoubles { get; set; } = 10;
+    
     public override TemplateItemProperties? OverrideProperties { get; set; } = new()
     {
         Prefab = new Prefab
@@ -42,98 +48,11 @@ public record Canted(HashSet<MongoId> filter) : NewItemFromCloneDetails
                 Required = false
             }
         ],
-        Accuracy = 0,
-        AdjustableOpticSensitivity = 0,
-        AdjustableOpticSensitivityMax = 0,
-        AnimationVariantsNumber = 0,
-        BackgroundColor = "blue",
-        BlocksCollapsible = false,
-        BlocksFolding = false,
-        CalibrationDistances = [[50, 100, 150, 200]],
-        CanPutIntoDuringTheRaid = true,
-        CanRequireOnRagfair = false,
-        CanSellOnRagfair = true,
-        CantRemoveFromSlotsDuringRaid = [],
-        ConflictingItems = 
-        [
-            ItemTpl.MOUNT_NCSTAR_MPR45_BACKUP
-        ],
-        CustomAimPlane = "",
-        DiscardLimit = -1,
-        DiscardingBlock = false,
-        DoubleActionAccuracyPenaltyMult = 1,
-        DropSoundType = ItemDropSoundType.None,
-        Durability = 100,
+        ConflictingItems = [ ItemTpl.MOUNT_NCSTAR_MPR45_BACKUP, _itemTpl ],
         EffectiveDistance = 0,
-        Ergonomics = -3,
-        ExamineExperience = 2,
-        ExamineTime = 1,
-        ExaminedByDefault = true,
-        ExtraSizeDown = 0,
-        ExtraSizeLeft = 0,
-        ExtraSizeRight = 0,
-        ExtraSizeUp = 0,
-        ExtraSizeForceAdd = false,
-        ForbidMissingVitalParts = false,
-        ForbidNonEmptyContainers = false,
-        Grids = [],
-        HasShoulderContact = false,
-        Height = 1,
-        HideEntrails = false,
-        InsuranceDisabled = false,
-        IsAdjustableOptic = false,
-        IsAlwaysAvailableForInsurance = false,
-        IsAnimated = false,
-        IsLockedAfterEquip = false,
-        IsSecretExitRequirement = false,
-        IsSpecialSlotOnly = false,
-        IsUnbuyable = false,
-        IsUndiscardable = false,
-        IsUngivable = false,
-        IsUnRemovable = false,
-        IsUnsaleable = false,
-        ItemSound = "mod",
-        LootExperience = 10,
-        Loudness = 0,
-        MergesWithChildren = false,
-        MetascoreGroup = "Firepower",
-        MinMaxFov = new XYZ
-        {
-            X = 0,
-            Y = 0,
-            Z = 0
-        },
-        QuestItem = false,
-        QuestStashMaxCount = 0,
-        RagFairCommissionModifier = 1,
-        RaidModdable = true,
-        RarityPvE = "Common",
-        Recoil = 0,
-        RepairCost = 0,
-        RepairSpeed = 0,
-        ScopesCount = 1,
-        SightingRange = 200,
-        StackMaxSize = 1,
-        StackObjectsCount = 1,
-        ToolModdable = true,
-        UniqueAnimationModID = 0,
-        Unlootable = false,
-        UnlootableFromSide = [],
-        UnlootableFromSlot = "FirstPrimaryWeapon",
-        Velocity = 0,
-        Weight = 0.113,
-        Width = 1,
-        ZoomSensitivity = 0,
-        Zooms = [[1, 1, 1, 1]],
-        SightModType = "reflex"
+        Ergonomics = 1,
     };
-
-    public override string? ParentId { get; set; } = "55818ad54bdc2ddc698b4569";
-    public override string? NewId { get; set; } = _itemTpl;
-    public override double? FleaPriceRoubles { get; set; } = 10;
-    public override double? HandbookPriceRoubles { get; set; } = 10;
-    public override string? HandbookParentId { get; set; } = "5b5f742686f774093e6cb4ff";
-
+    
     public override Dictionary<string, LocaleDetails>? Locales { get; set; } = new()
     {
         ["en"] = new LocaleDetails
